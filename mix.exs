de33ef2a7efc81a3762609f398a0e93e25e38fb9
John Davenport <johns10davenport@gmail.com>
2021-06-02 07:50:39 -0400

    Lose singleton

--- mix.exs
+++ mix.exs
@@ -26,10 +26,7 @@ defmodule ElixtronDesktop.MixProject do
   defp deps do
     [
       {:bakeware, "~>0.2.0"},
-      {:chrome_remote_interface, "~> 0.4.1"},
-      {:singleton, "~> 1.0.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:chrome_remote_interface, "~> 0.4.1"}
     ]
   end
   defp releases do
